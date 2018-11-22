eb5ac7045e1a8d9f1c3bfe19499b8c6bfa4eb59d
Adam M. Forte <forte.adam@gmail.com>
2018-11-22 08:16:33 -0600

    Update Mat2Arc
    Added option to Mat2Arc specify the file type for rasters output. Can choose between ESRI Ascii Grid or GeoTiff.

--- Compiled_Versions/cmpMfiles/cmpMat2Arc.m
+++ Compiled_Versions/cmpMfiles/cmpMat2Arc.m
@@ -1,4 +1,4 @@
-function cmpMat2Arc(wdir,MatFile,file_prefix)
+function cmpMat2Arc(wdir,MatFile,file_prefix,varargin)
 	% Description:
 	% 	Function converts all valid topotoolbox files contained within a mat file
 	%	to Arc compatible outputs. Specifically converts any GRIDobjs to
@@ -6,17 +6,44 @@ function cmpMat2Arc(wdir,MatFile,file_prefix)
 	%	flow direction grids saved as an ascii file, and any valid mapstructures
 	%	to shapefiles.
 	%
-	% Input:
+	% Required Inputs:
 	%	wdir - full path of working directory
 	%	MatFile - full path to matfile of interest
 	%	file_prefix - characters to add to the front of all output files
 	%
+	% Optional Inputs:
+	%	raster_type ['ascii'] - option to specify the format of the raster export, valid
+	%		inputs are 'tif' or 'ascii'
+	%
    	% Examples if running for the command line, minus OS specific way of calling main TAK function:
     %   Mat2Arc /path/to/wdir Topo.mat outputs
+    %	Mat2Arc /path/to/wdir Topo.mat outputs raster_type tif
 	%
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	% Function Written by Adam M. Forte - Updated : 07/02/18 %
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	if isdeployed
+		if ~isempty(varargin)
+			varargin=varargin{1};
+		end
+	end
+
+	% Parse Inputs
+	p = inputParser;
+	p.FunctionName = 'cmpMat2Arc';
+	addRequired(p,'wdir',@(x) ischar(x));
+	addRequired(p,'MatFile',@(x) ~isempty(regexp(x,regexptranslate('wildcard','*.mat'))));
+	addRequired(p,'file_prefix',@(x) ischar(x));
+
+	addParameter(p,'raster_type','ascii',@(x) ischar(validatestring(x,{'ascii','tif'})));
+
+	parse(p,wdir,MatFile,file_prefix,varargin{:});
+	wdir=p.Results.wdir;
+	MatFile=p.Results.MatFile;
+	file_prefix=p.Results.file_prefix;
+
+	raster_type=p.Results.raster_type;
+
 
 	MatFile=fullfile(wdir,MatFile);
 	d=whos('-file',MatFile);
@@ -30,8 +57,14 @@ function cmpMat2Arc(wdir,MatFile,file_prefix)
 
 		if strcmp(classOI,'GRIDobj')
 			varOI=load(mat_file,varNM);
-			out_name=fullfile(wdir,[file_prefix '_' varNM '.txt']);
-			GRIDobj2ascii(varOI.(varNM),out_name);
+			switch raster_type
+			case 'ascii'
+				out_name=[file_prefix '_' varNM '.txt'];
+				GRIDobj2ascii(varOI.(varNM),out_name);
+			case 'tif'
+				out_name=[file_prefix '_' varNM '.tif'];
+				GRIDobj2geotiff(varOI.(varNM),out_name);
+			end
 		elseif strcmp(classOI,'STREAMobj')
 			varOI=load(mat_file,varNM);
 			out_name=fullfile(wdir,[file_prefix '_' varNM '.shp']);
@@ -39,9 +72,15 @@ function cmpMat2Arc(wdir,MatFile,file_prefix)
 			shapewrite(MS,out_name);
 		elseif strcmp(classOI,'FLOWobj')
 			varOI=load(mat_file,varNM);
-			out_name=fullfile(wdir,[file_prefix '_' varNM '.txt']);
 			G=FLOWobj2GRIDobj(varOI.(varNM));
-			GRIDobj2ascii(G,out_name);
+			switch raster_type
+			case 'ascii'
+				out_name=[file_prefix '_' varNM '.txt'];
+				GRIDobj2ascii(G,out_name);
+			case 'tif'
+				out_name=[file_prefix '_' varNM '.tif'];
+				GRIDobj2geotiff(G,out_name);
+			end
 		elseif strcmp(classOI,'struct')
 			varOI=load(mat_file,varNM);
 			st=varOI.(varNM);
--- Mat2Arc.m
+++ Mat2Arc.m
@@ -1,4 +1,4 @@
-function Mat2Arc(mat_file,file_prefix)
+function Mat2Arc(mat_file,file_prefix,varargin)
 	%
 	% Usage:
 	%	Mat2Arc(mat_file,file_prefix);
@@ -10,9 +10,13 @@ function Mat2Arc(mat_file,file_prefix)
 	%	flow direction grids saved as an ascii file, and any valid mapstructures
 	%	to shapefiles.
 	%
-	% Input:
+	% Required Inputs:
 	%	mat_file - name or path to matfile of interest
 	%	file_prefix - characters to add to the front of all output files
+	%
+	% Optional Inputs:
+	%	raster_type ['ascii'] - option to specify the format of the raster export, valid
+	%		inputs are 'tif' or 'ascii'
 	%
 	% Example:
 	%	Mat file 'Data.mat' containing:
@@ -27,9 +31,29 @@ function Mat2Arc(mat_file,file_prefix)
 	%		Example_S.shp - shapefile of the streams in STREAMobj
 	%		Example_MS_KSN.shp - shapefile of the stream data with ksn
 	%
+	%	Mat2Arc('Data.mat','Example','raster_type','tif'); would produce:
+	%		Example_DEM.tif - GeoTiff of the DEM
+	%		Example_FD.tif - GeoTiff of the Arc flow direction grid
+	%		Example_S.shp - shapefile of the streams in STREAMobj
+	%		Example_MS_KSN.shp - shapefile of the stream data with ksn
+	%
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	% Function Written by Adam M. Forte - Updated : 06/18/18 %
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	% Parse Inputs
+	p = inputParser;
+	p.FunctionName = 'Mat2Arc';
+	addRequired(p,'mat_file',@(x) ~isempty(regexp(x,regexptranslate('wildcard','*.mat'))));
+	addRequired(p,'file_prefix',@(x) ischar(x));
+
+	addParameter(p,'raster_type','ascii',@(x) ischar(validatestring(x,{'ascii','tif'})));
+
+	parse(p,mat_file,file_prefix,varargin{:});
+	mat_file=p.Results.mat_file;
+	file_prefix=p.Results.file_prefix;
+
+	raster_type=p.Results.raster_type;
 
 	d=whos('-file',mat_file);
 
@@ -42,8 +66,14 @@ function Mat2Arc(mat_file,file_prefix)
 
 		if strcmp(classOI,'GRIDobj')
 			varOI=load(mat_file,varNM);
-			out_name=[file_prefix '_' varNM '.txt'];
-			GRIDobj2ascii(varOI.(varNM),out_name);
+			switch raster_type
+			case 'ascii'
+				out_name=[file_prefix '_' varNM '.txt'];
+				GRIDobj2ascii(varOI.(varNM),out_name);
+			case 'tif'
+				out_name=[file_prefix '_' varNM '.tif'];
+				GRIDobj2geotiff(varOI.(varNM),out_name);
+			end
 		elseif strcmp(classOI,'STREAMobj')
 			varOI=load(mat_file,varNM);
 			out_name=[file_prefix '_' varNM '.shp'];
@@ -51,9 +81,15 @@ function Mat2Arc(mat_file,file_prefix)
 			shapewrite(MS,out_name);
 		elseif strcmp(classOI,'FLOWobj')
 			varOI=load(mat_file,varNM);
-			out_name=[file_prefix '_' varNM '.txt'];
 			G=FLOWobj2GRIDobj(varOI.(varNM));
-			GRIDobj2ascii(G,out_name);
+			switch raster_type
+			case 'ascii'
+				out_name=[file_prefix '_' varNM '.txt'];
+				GRIDobj2ascii(G,out_name);
+			case 'tif'
+				out_name=[file_prefix '_' varNM '.tif'];
+				GRIDobj2geotiff(G,out_name);
+			end
 		elseif strcmp(classOI,'struct')
 			varOI=load(mat_file,varNM);
 			st=varOI.(varNM);
